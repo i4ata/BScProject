e8cd37591922753b749ba9baa0be0c6cc7ba48dd
a <a@a.com>
2023-07-13 23:39:37 +0200

    runs

---
+++ ppo_negotiation_simplified/jobscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=30:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=marl_experiments
+#SBATCH --mem=8000
+
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+module load tqdm/4.64.0-GCCcore-11.3.0
+module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
+
+python main.py
